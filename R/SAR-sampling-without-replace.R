4638940bcce13db45ae432f22a1f894c0a8a7196
Jfannyx <693469124@qq.com>
2023-06-21 19:47:58 +0800

    add linear correlation

--- R/SAR-sampling-without-replace.R
+++ R/SAR-sampling-without-replace.R
@@ -1,4 +1,5 @@
 library(permute)
+set.seed(100001)
 rm(list=ls())
 gc()
 
@@ -56,6 +57,57 @@ result.c
 
 hist(result.z[,1]) # many z bigger than 1
 
+d$nitrogenPercent[is.nan(d$nitrogenPercent)] = NA
+d$organicCPercent[is.nan(d$organicCPercent)] = NA
+d_site <- aggregate(cbind(d$soilInCaClpH,d$nitrogenPercent,d$organicCPercent,
+                          d$soilMoisture, d$map_mm,d$prec_seasonality,d$mat_celsius,
+                          d$temp_seasonality),list(d$Site), function(x) mean(x,na.rm = T))
+d_site$V2[is.nan(d_site$V2)] = NA
+d_site$V3[is.nan(d_site$V3)] = NA
+row.names(d_site) <- d_site$Group.1
+d_site <- d_site[,-1]
+colnames(d_site) <- c("soilInCaClpH","nitrogenPercent","organicCPercent",
+                      "soilMoisture", "map_mm",'prec_seasonality',"mat_celsius",
+                      "temp_seasonality")
+# standardization seems to have no effect on significance
+d_site <- d_site[a,]
+d_site <- as.data.frame(scale(d_site))
+summary(lm(result.z[,1] ~ d_site$soilInCaClpH + d_site$soilMoisture + d_site$mat_celsius +
+             d_site$map_mm + d_site$temp_seasonality + d_site$prec_seasonality))
+
+# data with no missing values
+summary(lm(scale(result.z[,1]) ~ d_site$mat_celsius +
+             d_site$map_mm + d_site$temp_seasonality + d_site$prec_seasonality))
+
+library(ggplot2)
+library(ggtrendline)
+library(gridExtra)
+p1 <- ggtrendline(d_site$soilInCaClpH, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$soilInCaClpH, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("soilInCaClpH")+ylab("z")
+
+p2 <- ggtrendline(d_site$soilMoisture, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$soilMoisture, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("soilMoisture")+ylab("z")
+
+p3 <- ggtrendline(d_site$mat_celsius, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$mat_celsius, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("mat_celsius")+ylab("z")
+
+p4 <- ggtrendline(d_site$map_mm, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$map_mm, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("map_mm")+ylab("z")
+
+p5 <- ggtrendline(d_site$temp_seasonality, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$temp_seasonality, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("temp_seasonality")+ylab("z") 
+
+p6 <- ggtrendline(d_site$prec_seasonality, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$prec_seasonality, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("prec_seasonality")+ylab("z") 
+
+grid.arrange(p1,p2,p3,p4,p5,p6,ncol = 3)
+
 #####
 #Dob#
 
@@ -69,9 +121,9 @@ a1 <- unique(d1$Site)
 result.c <- matrix(nrow = 68, ncol = 4)
 result.z <- matrix(nrow = 68, ncol = 4)
 
-for (i in 1:length(a)){
+for (i in 1:length(a1)){
   # take out one site
-  dob_sub <- subset_samples(dob, Site==a[i])
+  dob_sub <- subset_samples(dob, Site==a1[i])
   dim1 <- dim(otu_table(dob_sub)) # the number of samples in one site
   species <- vector(length = dim1[1]) # create a vector to save diversity
   
@@ -105,3 +157,54 @@ result.z
 result.c
 
 hist(result.z[,1]) # many z bigger than 1
+
+d1$nitrogenPercent[is.nan(d1$nitrogenPercent)] = NA
+d1$organicCPercent[is.nan(d1$organicCPercent)] = NA
+d1$map_mm[is.nan(d1$map_mm)] = NA
+
+d_site <- aggregate(cbind(d1$soilInCaClpH,d1$nitrogenPercent,d1$organicCPercent,
+                          d1$soilMoisture, d1$map_mm,d1$prec_seasonality,d1$mat_celsius,
+                          d1$temp_seasonality),list(d1$Site), function(x) mean(x,na.rm = T))
+d_site$V2[is.nan(d_site$V2)] = NA
+d_site$V3[is.nan(d_site$V3)] = NA
+d_site$V4[is.nan(d_site$V4)] = NA
+row.names(d_site) <- d_site$Group.1
+d_site <- data.frame(scale(d_site[,-1]))
+colnames(d_site) <- c("soilInCaClpH","nitrogenPercent","organicCPercent",
+                      "soilMoisture", "map_mm",'prec_seasonality',"mat_celsius",
+                      "temp_seasonality")
+# standardization seems to have no effect on significance
+d_site <- d_site[a1,]
+summary(lm(result.z[,1] ~ d_site$soilInCaClpH + d_site$soilMoisture + d_site$mat_celsius +
+             d_site$map_mm + d_site$temp_seasonality))
+
+# data with no missing values
+
+summary(lm(result.z[,1] ~ d_site$mat_celsius +
+             d_site$map_mm + d_site$temp_seasonality + d_site$prec_seasonality))
+
+p1 <- ggtrendline(d_site$soilInCaClpH, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$soilInCaClpH, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("soilInCaClpH")+ylab("z")
+
+p2 <- ggtrendline(d_site$soilMoisture, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$soilMoisture, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("soilMoisture")+ylab("z")
+
+p3 <- ggtrendline(d_site$mat_celsius, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$mat_celsius, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("mat_celsius")+ylab("z")
+
+p4 <- ggtrendline(d_site$map_mm, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$map_mm, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("map_mm")+ylab("z")
+
+p5 <- ggtrendline(d_site$temp_seasonality, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$temp_seasonality, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("temp_seasonality")+ylab("z") 
+
+p6 <- ggtrendline(d_site$prec_seasonality, result.z[,1],linecolor="red")+
+  geom_point(aes(d_site$prec_seasonality, result.z[,1]),color="black",pch=21,fill='white')+
+  xlab("prec_seasonality")+ylab("z") 
+
+grid.arrange(p1,p2,p3,p4,p5,p6,ncol = 3)
